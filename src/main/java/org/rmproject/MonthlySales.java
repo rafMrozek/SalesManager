9e965293ce5d2c5291c7a3bfa9ee74942f55cfb2
Rafal Mrozek <raf.mrozek@gmail.com>
2023-10-10 22:37:24 +0200

    Poprawiono Monthly Sales (Koszty, Sumowanie cen)

--- src/main/java/org/rmproject/MonthlySales.java
+++ src/main/java/org/rmproject/MonthlySales.java
@@ -3,43 +3,32 @@ package org.rmproject;
 import java.sql.*;
 import java.util.Scanner;
 
-import static com.sun.org.apache.xpath.internal.XPath.SELECT;
-
 public class MonthlySales {
     public static void addCosts(Scanner scanner) {
         System.out.println("Podaj swoje koszty: ");
         double costs = Double.parseDouble(scanner.nextLine());
     }
     public static void sumValues() {
-
-        String jdbcUrl = "jdbc:mysql://localhost:3306/rmp";  //
-        String username = "root";
-        String password = "";
-
-        double purchasePrice = Double.parseDouble(("purchasePrice"));
-        double price = Double.parseDouble(("price"));
-
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/rmp", "root", "");
 
-            String query = "UPDATE orders SET priceFromOffers = (SELECT purchasePrice FROM offers WHERE offers.name = orders.name)";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            String updateQuery = "UPDATE orders SET priceFromOffers = (SELECT purchasePrice FROM offers WHERE offers.name = orders.name)";
+            PreparedStatement preparedStatement = connection.prepareStatement(updateQuery);
 
             int rowsAffected = preparedStatement.executeUpdate();
             System.out.println("Liczba zmienionych wierszy: " + rowsAffected);
 
-            ResultSet resultSet = preparedStatement.executeQuery(query);
-            String query1 = "SELECT SUM(" + purchasePrice + ")"; //, SUM(" + price + ") FROM orders");
+            String sumQuery = "SELECT SUM(priceFromOffers) as sumPurchasePrice, SUM(price) as sumSellingPrice FROM orders";
+            ResultSet resultSet = preparedStatement.executeQuery(sumQuery);
 
-            ResultSet resultSet1 = preparedStatement.executeQuery(query1);
-
-            if (resultSet1.next()) {
-                double sumPurchasePrice = resultSet1.getDouble("purchasePrice");
-                double sumSellingPrice = resultSet1.getDouble("price");
+            if (resultSet.next()) {
+                double sumPurchasePrice = resultSet.getDouble("sumPurchasePrice");
+                double sumSellingPrice = resultSet.getDouble("sumSellingPrice");
+                System.out.println("Suma cen zakupu: " + sumPurchasePrice);
+                System.out.println("Suma cen sprzedaży: " + sumSellingPrice);
             }
 
             resultSet.close();
-            resultSet1.close();
             preparedStatement.close();
             connection.close();
         } catch (Exception e) {
@@ -47,5 +36,6 @@ public class MonthlySales {
         }
     }
 }
+
 
 
